185d89531efdef5f9ba9053b1d1f859b1ac6e258
YamanAyoun <yamanayoun0@gmail.com>
2023-05-03 18:52:42 +0000

    add some routs for server

--- server.js
+++ server.js
@@ -1,47 +1,162 @@
 const express = require('express');
-
 const server = express();
-
+const axios = require('axios');
 const PORT = 3000;
-
+const cors = require('cors');
+require('dotenv').config();
+const APIKey = process.env.APIKey;
+server.use(cors())
 const data = require('./Movie Data/data.json');
 
+
+server.get('/', homeHandler)
+server.get('/favorite', favoriteHandler)
+server.get('/error', Error500)
+// server.get('*', Error400)
+server.get('/trending', trendingHandler)
+server.get('/search', searchHandler)
+server.get('/genres', genresHandler)
+server.get('/popular', popularHandler)
+
+function homeHandler(req, res) {
+  let home = new movie(data.title, data.poster_path, data.overview)
+
+  res.status(200).send(home);
+  ;
+}
+
+function movie(title, poster_path, overview) {
+  this.title = title,
+    this.poster_path = poster_path,
+    this.overview = overview
+
+}
+
+function favoriteHandler(req, res) {
+  str = 'Welcome to Favorite Page'
+  res.status(200).send(str)
+}
+
+function Error500(req, res) {
+  let err5 = {
+    "status": 500,
+
+    "responseText": 'Sorry, something went wrong'
+  }
+  res.status(err5.status).send(err5);
+};
+
+function Error400(req, res) {
+  res.status(404).send('page not found error')
+};
+
+
+function trendingHandler(req, res) {
+  const url = `https://api.themoviedb.org/3/trending/all/week?api_key=${APIKey}`
+
+  axios.get(url)
+    .then(result => {
+
+      let mapResult = result.data.results.map(item => {
+        let singleResult = new dataTrending(item.id, item.title, item.release_date, item.poster_path, item.overview);
+        return singleResult;
+      })
+      console.log(mapResult)
+      res.send(mapResult)
+
+    })
+
+    .catch((error) => {
+      console.log('sorry you have something error', error)
+      res.status(500).send(error);
+    })
+
+};
+
+function searchHandler(req, res) {
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=${APIKey}&language=en-US&query=The&page=2`
+
+  axios.get(url)
+    .then(result => {
+      let mapResult = result.data.results.map(item => {
+        let singleResult = new dataSearch(item.id, item.title);
+        return singleResult;
+      })
+      console.log(mapResult)
+      res.send(mapResult)
+
+    })
+    .catch((error) => {
+      console.log('sorry you have something error', error)
+      res.status(500).send(error);
+    })
+}
+
+
+function genresHandler(req, res) {
+  const url = `https://api.themoviedb.org/3/genre/movie/list?api_key=fd0e5badbd55e349db3527d6cef8ad87&language=en-US`
+
+  axios.get(url)
+    .then(result => {
+      let mapResult = result.data.genres.map(item => {
+        let singleResult = new dataGenres(item.id, item.name);
+        return singleResult;
+      })
+      console.log(mapResult)
+      res.send(mapResult)
+
+    })
+    .catch((error) => {
+      console.log('sorry you have something error', error)
+      res.status(500).send(error);
+    })
+
+}
+
+function popularHandler(req, res) {
+  const url = `https://api.themoviedb.org/3/person/popular?api_key=fd0e5badbd55e349db3527d6cef8ad87&language=en-US&page=1`
+
+  axios.get(url)
+    .then(result => {
+      let mapResult = result.data.results.map(item => {
+        let singleResult = new dataPopular(item.name, item.gender);
+        return singleResult;
+      })
+      console.log(mapResult)
+      res.send(mapResult)
+
+    })
+    .catch((error) => {
+      console.log('sorry you have something error', error)
+      res.status(500).send(error);
+    })
+
+}
+
+
+function dataTrending(id, title, release_date, poster_path, overview) {
+  this.id = id;
+  this.title = title;
+  this.release_date = release_date;
+  this.poster_path = poster_path;
+  this.overview = overview;
+}
+
+function dataSearch(id, title) {
+  this.id = id;
+  this.title = title;
+}
+
+function dataGenres(id, name) {
+  this.id = id;
+  this.name = name;
+}
+
+function dataPopular(name ,gender) {
+  this.name = name;
+  this.gender = gender;
+}
+
 server.listen(PORT, () => {
-    console.log(`Server port ${PORT}`);
-  });
-  
-  
-  server.get('/', (req, res) => {
-    let home={
-        "title": data.title,
-        "poster_path": data.poster_path,
-        "overview": data.overview
-    }
-    res.send(home);
-  });
-  
-  server.get('/favorite', (req, res) => {
-    res.send('Welcome to Favorite Page');
-  });
-  
-  
-  server.get('/error' ,(req, res) => {
-    let err5={
-        "status": 500,
-
-        "responseText" : 'Sorry, something went wrong'
-    }
-    res.status(err5.status).send(err5);
-  });
-  
-  
-  server.get((req, res) => {
-    let err4={
-        "status": 404,
-
-        "responseText" : 'page not found error'
-    }
-    res.status(err4.status).send(err4);
-  });
-  
-  
+  console.log(`Listening on ${PORT}: I'm ready`);
+});
